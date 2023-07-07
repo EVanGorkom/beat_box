04e90be612956fba4a8038a95e8b9b2a5ca28e4d
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-06 22:49:23 -0500

    Build the node class and respective test

---
+++ spec/node_spec.rb
@@ -0,0 +1,23 @@
+require "rspec"
+require "./lib/node.rb"
+
+RSpec.describe Node do
+    it "exists" do
+        node = Node.new("plop")
+
+        expect(node).to be_an_instance_of(Node)
+    end
+
+    it "has readable attributes" do
+        node = Node.new("plop")
+
+        expect(node.data).to eq("plop")
+    end
+
+    it "has spce for a following node" do
+        node = Node.new("plop")
+
+        expect(node.next_node).to eq()
+    end
+
+end
